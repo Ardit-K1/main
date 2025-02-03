84306a91d5fad8edb94af6e01b9504b1a6067df1
Ardit-K1 <arditk2077@gmail.com>
2025-02-03 03:11:31 -0800

    u

---
+++ test/main.js
@@ -0,0 +1,52 @@
+var a=10;
+var b=20;
+var c=a+b;
+console.log(c);
+
+var d=10;
+var e=4;
+var f=d%e;
+console.log(f);
+
+var g=99;
+g++;
+console.log(g);
+
+var h=109;
+h--;
+console.log(h);
+
+var i=20;
+var j=2;
+var k= i**j;
+console.log(k);
+
+var x = 15;
+x += 5;
+console.log(x);
+
+var l=100;
+var m=10;
+console.log(l != m)
+
+var n=1234;
+var o="1234";
+console.log(n === o);
+
+var var1=20;
+console.log(var1 <= 18 && var1 <=50);
+
+var2=4;
+var3=6;
+
+console.log(var2 == 4 || var3 ==4);
+
+var4=9;
+var5=10;
+console.log(!(var4 == var5));
+
+function showMessage(){
+    alert("Klasa ma e mira");
+}
+showMessage();
+
